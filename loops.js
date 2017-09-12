d7954f12779be018397556a029970899172fe24f
Luke Borsare <lborsare1@gmail.com>
2017-09-12 23:51:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,12 +2,7 @@
 function forLoop(array) {
   var array = []
   for (let i = 0; i<26; i++){
-    if (i==1) {
       array.push("I am 1 strange loop.")
-    }
-    else {
-      array.push(`I am ${i} strange loops.`)
-    }
   }
   return [array]
 }

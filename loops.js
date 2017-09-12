68e0ee22999c29173214b53568bc13a432cd093b
Luke Borsare <lborsare1@gmail.com>
2017-09-12 23:55:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 function forLoop(array) {
   array = []
   for (let i = 0; i<25; i++){
-      array.push(`I am ${i} strange loop.`)
+      array.push(`I am ${i} strange loops.`)
   }
   return [array]
 }

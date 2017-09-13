498e5cd3cde5aead404d5d423aa09829fb8e44aa
Luke Borsare <lborsare1@gmail.com>
2017-09-13 00:21:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,6 +11,9 @@ function maybeTrue() {
   return Math.random() >= 0.5
 }
 function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
   do {
     array.pop()
   } while (array.length > 0 && maybeTrue())
